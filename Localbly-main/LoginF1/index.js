20a17365a3b8b7e9e1b552465b9b62f444b9c61e
Konectify <109717935+Konectify@users.noreply.github.com>
2023-01-26 14:48:33 +0300

    Update index.js

--- Localbly-main/LoginF1/index.js
+++ Localbly-main/LoginF1/index.js
@@ -9,34 +9,36 @@ async function Login(context,req){
   const user = await fn.findOne({ email: body.email }, 'businesses');
   const permissions = 'c';
   const container = 'images';
+try{
+      if (user) {
+        // check user password with hashed password stored in the database
+        const validPassword = await bcrypt.compare(body.password, user.password);
+        if (validPassword) {
+          const token = auth.generateAccessToken(user.email);
+          const id = user._id;
 
-  if (user) {
-    // check user password with hashed password stored in the database
-    const validPassword = await bcrypt.compare(body.password, user.password);
-    if (validPassword) {
-      const token = auth.generateAccessToken(user.email);
-      const id = user._id;
+          let bizname= user.bizname;
+          let location = user.location;
+          let name = user.name;
+          const imgKeys = {body:auth.generateSasToken(process.env.AzureWebJobsStorage, container, permissions)}
 
-      let bizname= user.bizname;
-      let location = user.location;
-      let name = user.name;
-      const imgKeys = {body:auth.generateSasToken(process.env.AzureWebJobsStorage, container, permissions)}
+          context.res.status(200).json({ message: "Valid password", 
 
-      context.res.status(200).json({ message: "Valid password", 
-                                          
-                                           location:location,
-                                           username:name,
-                                           bizname:bizname,
-                                           token:token,
-                                           id:id,
-                                           code:0,
-                                           accessKeys: imgKeys.body
-                                          });
-    } else {
-      context.res.status(400).json({ error: "Invalid Password", code:2 });
-    }
-  } else {
-    context.res.status(401).json({ error: "User does not exist", code:2});
-  }
+                                               location:location,
+                                               username:name,
+                                               bizname:bizname,
+                                               token:token,
+                                               id:id,
+                                               code:0,
+                                               accessKeys: imgKeys.body
+                                              });
+        } else {
+          context.res.status(400).json({ error: "Invalid Password", code:2 });
+        }
+      } else {
+        context.res.status(401).json({ error: "User does not exist", code:2});
+      }
+ }catch(error)
+   context.res.status(500).json({ error: error.message});
 }
 module.exports = {Login}
